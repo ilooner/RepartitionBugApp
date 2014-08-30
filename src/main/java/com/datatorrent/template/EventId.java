84666968220cd2f9b75414f2eb8227b9717826bf
Timothy Farkas <tim@datatorrent.com>
2014-08-29 17:12:42 -0700

    Used basic counters.

--- src/main/java/com/datatorrent/template/EventId.java
+++ src/main/java/com/datatorrent/template/EventId.java
@@ -1,32 +1,24 @@
 package com.datatorrent.template;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class EventId
 {
+
   public long batchId;
   public long tupleId;
   public long windowId;
   public long partitionId;
   public long operatorId;
   public long recieveWindowId;
-  
+
   @Override
   public String toString()
   {
-    StringBuilder sb = new StringBuilder();
-    sb.append("batchId: ");
-    sb.append(batchId);
-    sb.append(" tupleId: ");
-    sb.append(tupleId);
-    sb.append(" windowId: ");
-    sb.append(windowId);
-    sb.append(" partitionId: ");
-    sb.append(partitionId);
-    sb.append(" operatorId: ");
-    sb.append(operatorId);
-    sb.append(" recieveWindowId: ");
-    sb.append(recieveWindowId);
-    sb.append("\n");
-    
-    return sb.toString();
+    return batchId + ", tupleId=" + tupleId
+      + ", windowId=" + Long.toHexString(windowId) + ", partitionId=" + partitionId + ", operatorId=" + operatorId + ", recieveWindowId=" + Long.toHexString(recieveWindowId) + '}';
   }
+
+  private static final Logger logger = LoggerFactory.getLogger(EventId.class);
 }
