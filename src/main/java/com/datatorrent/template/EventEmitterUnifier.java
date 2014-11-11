7e23fc7fea095cc3ec0c02fd8c6c80826ee2e2cc
gaurav <gaurav@datatorrent.com>
2014-11-11 12:26:04 -0800

    added more checks in unifier

--- src/main/java/com/datatorrent/template/EventEmitterUnifier.java
+++ src/main/java/com/datatorrent/template/EventEmitterUnifier.java
@@ -16,11 +16,16 @@ public class EventEmitterUnifier implements Operator.Unifier<EventId>
   private long tupleCounter = 0;
   private static final Logger LOG = LoggerFactory.getLogger(EventEmitterUnifier.class);
   public final transient DefaultOutputPort<EventId> output = new DefaultOutputPort<EventId>();
+  private transient boolean firstTuple = true;
 
   @Override
   public void process(EventId tuple)
   {
     tupleCounter++;
+    if(firstTuple){
+      firstTuple = false;
+      LOG.info("window id {} of the tuple and window id {} of the operator",tuple.windowId, windowId);
+    }
     output.emit(tuple);
   }
 
@@ -28,11 +33,13 @@ public class EventEmitterUnifier implements Operator.Unifier<EventId>
   public void beginWindow(long windowId)
   {
     this.windowId = windowId;
+    firstTuple = true;
   }
 
   @Override
   public void endWindow()
   {
+
     LOG.info("total tuples {} in window id {}", tupleCounter, windowId);
   }
 
